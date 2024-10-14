dfaccbb7deef8cdd180110ff54423c3bb5ecbdfd
avivmor97 <aviv.mor97@gmail.com>
2024-10-14 13:06:10 +0300

    Add note.service.js

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,88 @@
-// note service
+import { utilService } from '../services/util.service.js'
+
+
+export const noteService = {
+    getNotes,
+    createNote,
+    updateNote,
+    deleteNote,
+    duplicateNote
+
+
+
+}
+
+const notes = [
+    {
+      id: 'n101',
+      createdAt: Date.now(),
+      type: 'NoteTxt',
+      isPinned: true,
+      style: {
+        backgroundColor: '#00d'
+      },
+      info: {
+        txt: 'Fullstack Me Baby!'
+      }
+    },
+
+    {
+        id: 'n102',
+        createdAt: Date.now(),
+        type: 'NoteTxt',
+        isPinned: true,
+        style: {
+          backgroundColor: '#00d'
+        },
+        info: {
+          txt: 'Fullstack Me Baby!'
+        }
+      },
+
+      {
+        id: 'n103',
+        createdAt: Date.now(),
+        type: 'NoteTxt',
+        isPinned: true,
+        style: {
+          backgroundColor: '#00d'
+        },
+        info: {
+          txt: 'Fullstack Me Baby!'
+        }
+      }
+
+  ]
+
+
+  function getNotes() {
+    return notes
+  }
+  
+  function createNote(note) {
+    note.id = utilService.makeId()
+    note.createdAt = Date.now()
+    note.style.backgroundColor = utilService.getRandomColor()
+    notes.push(note)
+  }
+  
+  
+  function updateNote(noteId, updatedNote) {
+    const idx = notes.findIndex(note => note.id === noteId)
+    if (idx !== -1) notes[idx] = updatedNote
+  }
+  
+  function deleteNote(noteId) {
+    const idx = notes.findIndex(note => note.id === noteId)
+    if (idx !== -1) notes.splice(idx, 1)
+  }
+  
+  function duplicateNote(noteId) {
+    const note = notes.find(note => note.id === noteId)
+    if (note) {
+      const newNote = { ...note, id: utilService.makeId() }
+      notes.push(newNote)
+    }
+  }
+
+
