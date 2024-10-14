f87397945bd78c32698868c6b764aaa77e4ff11f
LeonPr <leon@masig.co.il>
2024-10-14 18:20:58 +0300

    Add nav to email list

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -2,16 +2,25 @@
 
 
 export function MailList({ mails }) {
-    return <section>
-        
-        <h2>Inbox</h2>
-        <table >
-            {mails.map(mail => (
-                <tbody>
-                    <tr key={mail.id}>
-                        <td >{mail.subject}</td>
-                        <td >{mail.body}</td>
-                    </tr></tbody>))}
-        </table>
-    </section>
+    return (
+        <section className="mail-list">
+            <button className="new-email">Compose</button>
+            <ul className="side-nav">
+                <li>Inbox</li>
+                <li>Starred</li>
+                <li>Important</li>
+                <li>Sent</li>
+                <li>Draft</li>
+                <li>Categories</li>
+            </ul>
+            <table className="mail-table">
+                {mails.map(mail => (
+                    <tbody>
+                        <tr key={mail.id}>
+                            <td >{mail.subject}</td>
+                            <td >{mail.body}</td>
+                        </tr></tbody>))}
+            </table>
+        </section>
+    )
 }
