9efb7c803fe72751138f5742d737e22f1d4ab836
avivmor97 <aviv.mor97@gmail.com>
2024-10-18 11:22:43 +0300

    dsigned Hompage

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,9 @@
-
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to Leon & Aviv home page!</h1>
-    </section>
+    return (
+        <section className="home">
+            <h1>Welcome to Leon & Aviv Home Page!</h1>
+            <img className="home-img" src="assets\img\Gmail_Icon.png" alt="Image 1" />
+            <img className="home-img" src="assets\img\google keep icon.png" alt="Image 2" />
+        </section>
+    );
 }
