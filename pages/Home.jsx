100e54ebf3b0a845da19eccf15fef7994ae56307
avivmor97 <aviv.mor97@gmail.com>
2024-10-18 11:58:22 +0300

    clicking on Imgs in homepage will send you to thier destenations

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,9 +1,15 @@
+const { Link } = ReactRouterDOM;
+
 export function Home() {
     return (
         <section className="home">
             <h1>Welcome to Leon & Aviv Home Page!</h1>
-            <img className="home-img" src="assets\img\Gmail_Icon.png" alt="Image 1" />
-            <img className="home-img" src="assets\img\google keep icon.png" alt="Image 2" />
+            <Link to="/mail">
+                <img className="home-img" src="assets/img/Gmail_Icon.png" alt="Image 1" />
+            </Link>
+            <Link to="/note">
+                <img className="home-img" src="assets/img/google keep icon.png" alt="Image 2" />
+            </Link>
         </section>
     );
 }
